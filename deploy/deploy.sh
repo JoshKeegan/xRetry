b78a50b005f43f233e8a174c7b8f4d860f83e43c
Josh Keegan <Josh.Keegan@dominos.co.uk>
2019-05-08 12:18:37 +0100

    Fix release: Exclude symbols from nuget package

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,4 +6,9 @@
 
 apiKey=$1
 
-dotnet nuget push --source https://api.nuget.org/v3/index.json --api-key $apiKey ../artefacts/nuget/\*.nupkg
+# Must explicitly say there are no symbols for the nuget package, or it will error saying there aren't any accompanying *.snupkg files
+dotnet nuget push \
+	--source https://api.nuget.org/v3/index.json \
+	--api-key $apiKey \
+	--no-symbols ../artefacts/nuget/\*.nupkg \
+	../artefacts/nuget/\*.nupkg
