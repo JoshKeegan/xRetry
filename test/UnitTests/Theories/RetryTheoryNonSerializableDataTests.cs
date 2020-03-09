a4310790ea70950608a2bc9b6105227b64405b80
Josh Keegan <Josh.Keegan@dominos.co.uk>
2020-03-09 18:26:16 +0000

    Add test case for retrying theories that cannot be enumerated at compile time

---
+++ test/UnitTests/Theories/RetryTheoryNonSerializableDataTests.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using xRetry;
+using Xunit;
+
+namespace UnitTests.Theories
+{
+    // Non-serializable test data is a special case during xunit test discovery.
+    //  These usually get discovered at runtime. See https://github.com/xunit/xunit/blob/3f3f6929c301db40e9d5d8ced2bb494084f57c82/src/xunit.execution/Sdk/Frameworks/TheoryDiscoverer.cs#L75
+    //  Note that this isn't the only case handled by that method as there are other cases that cannot be pre-enumerated,
+    // but this test should prove we have a working implementation of CreateTestCasesForTheory, so gives high confidence
+    // without worrying about every possible edge case.
+    public class RetryTheoryNonSerializableDataTests
+    {
+        // testId => numCalls
+        private static readonly Dictionary<int, int> defaultNumCalls = new Dictionary<int, int>()
+        {
+            { 0, 0 },
+            { 1, 0 }
+        };
+
+        [RetryTheory]
+        [MemberData(nameof(GetTestData))]
+        public void Default_Reaches3(NonSerializableTestData nonSerializableWrapper)
+        {
+            defaultNumCalls[nonSerializableWrapper.Id]++;
+
+            Assert.Equal(3, defaultNumCalls[nonSerializableWrapper.Id]);
+        }
+
+        public static IEnumerable<object[]> GetTestData() => new[]
+        {
+            new object[] { new NonSerializableTestData(0) },
+            new object[] { new NonSerializableTestData(1) }
+        };
+
+        public class NonSerializableTestData
+        {
+            public readonly int Id;
+
+            public NonSerializableTestData(int id)
+            {
+                Id = id;
+            }
+        }
+    }
+}
