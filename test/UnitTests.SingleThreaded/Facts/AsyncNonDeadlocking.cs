a8f144867a820e335d10659b7d2facf028f9c456
Josh Keegan <josh.keegan@gmx.com>
2020-04-10 15:51:07 +0100

    Add new UnitTests.SingleThreaded project, and move the tests that need to be run single-threaded into there

--- test/UnitTests.SingleThreaded/Facts/AsyncNonDeadlocking.cs
+++ test/UnitTests.SingleThreaded/Facts/AsyncNonDeadlocking.cs
@@ -1,15 +1,10 @@
 using System.Threading.Tasks;
 using xRetry;
 
-namespace UnitTests.Facts
+namespace UnitTests.SingleThreaded.Facts
 {
     public class AsyncNonDeadlocking
     {
-        // Usage: set maxParallelThreads to 1 in xunit.runner.json and run this test to check if it passes
-        // TODO: Can xunit be told to run this specific test with that setting?
-        //  If not, perhaps make this its own test assembly so we can have a separate suite of tests with maxParallelThreads 1
-        //  Or just script running tests with and without that set during build, so we run all tests in both configurations
-
         [RetryFact(1)]
         public async Task AwaitWithinAsyncTask_RunsToCompletion()
         {
