28678f670d77b3ae32db4ad70e6ef6e004553063
Josh Keegan <josh.keegan@gmx.com>
2021-05-25 20:46:58 +0100

    Change deploy to be ready for an actual release . . .

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,11 +6,6 @@
 
 apiKey=$1
 
-# TODO: Remove once actions are working as intended
-echo "Here! API Key is ${#apiKey} chars long"
-ls ../artefacts
-exit 1
-
 # Must explicitly say there are no symbols for the nuget package, or it will error saying there aren't any accompanying *.snupkg files
 dotnet nuget push \
 	--source https://api.nuget.org/v3/index.json \
