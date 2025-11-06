ac9561355cf9948d72578726959f7663c4c1e2db
Josh Keegan <josh.keegan@gmx.com>
2025-11-06 17:44:51 +0000

    Revert "v3: Temporarily remove tests for non-serialisable data"
    This reverts commit 75e9d5b9f3eaf37c4e8c11ccf2f8da805bfd89a5.

--- test/UnitTests.v3/Theories/RetryTheoryRuntimeSkipNonSerializableDataTests.cs
+++ test/UnitTests.v3/Theories/RetryTheoryRuntimeSkipNonSerializableDataTests.cs
@@ -6,39 +6,38 @@ using Xunit;
 
 namespace UnitTests.v3.Theories
 {
-    // TODO: v3 - missing feature
-    // public class RetryTheoryRuntimeSkipNonSerializableDataTests
-    // {
-    //     [RetryTheory(SkipExceptions = new[] { typeof(TestException) })]
-    //     [MemberData(nameof(GetTestData))]
-    //     public void CustomException_SkipsAtRuntime(NonSerializableTestData _)
-    //     {
-    //         throw new TestException();
-    //     }
-    //
-    //     // testId => numCalls
-    //     private static readonly Dictionary<int, int> skippedNumCalls = new Dictionary<int, int>()
-    //     {
-    //         { 0, 0 },
-    //         { 1, 0 }
-    //     };
-    //
-    //     [RetryTheory]
-    //     [MemberData(nameof(GetTestData))]
-    //     public void Skip_DoesNotRetry(NonSerializableTestData nonSerializableWrapper)
-    //     {
-    //         // Assertion would fail on subsequent attempts, before reaching the skip
-    //         skippedNumCalls[nonSerializableWrapper.Id]++;
-    //
-    //         skippedNumCalls[nonSerializableWrapper.Id].Should().Be(1);
-    //
-    //         Assert.Skip("some reason");
-    //     }
-    //
-    //     public static IEnumerable<object[]> GetTestData() => new[]
-    //     {
-    //         new object[] { new NonSerializableTestData(0) },
-    //         new object[] { new NonSerializableTestData(1) }
-    //     };
-    // }
+    public class RetryTheoryRuntimeSkipNonSerializableDataTests
+    {
+        [RetryTheory(SkipExceptions = new[] { typeof(TestException) })]
+        [MemberData(nameof(GetTestData))]
+        public void CustomException_SkipsAtRuntime(NonSerializableTestData _)
+        {
+            throw new TestException();
+        }
+
+        // testId => numCalls
+        private static readonly Dictionary<int, int> skippedNumCalls = new Dictionary<int, int>()
+        {
+            { 0, 0 },
+            { 1, 0 }
+        };
+
+        [RetryTheory]
+        [MemberData(nameof(GetTestData))]
+        public void Skip_DoesNotRetry(NonSerializableTestData nonSerializableWrapper)
+        {
+            // Assertion would fail on subsequent attempts, before reaching the skip
+            skippedNumCalls[nonSerializableWrapper.Id]++;
+
+            skippedNumCalls[nonSerializableWrapper.Id].Should().Be(1);
+
+            Assert.Skip("some reason");
+        }
+
+        public static IEnumerable<object[]> GetTestData() => new[]
+        {
+            new object[] { new NonSerializableTestData(0) },
+            new object[] { new NonSerializableTestData(1) }
+        };
+    }
 }
